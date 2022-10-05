b2108b624caee2a6e55765cfbff32a5ee59b1a52
JamesSenva <jamessenva@gmail.com>
2022-10-05 23:12:11 +0530

    Add alternative solution using switch statement

--- chapter03/js/following-day.js
+++ chapter03/js/following-day.js
@@ -7,20 +7,46 @@
 let dayName = prompt('Tell me the day?');
 dayName = dayName.toLowerCase();
 
-if(dayName === 'sunday'){
-            alert('Tomorrow is Monday');
-    }   else if(dayName === 'monday'){
-            alert('Tomorrow is Tuesday!');
-    }   else if(dayName === 'tuesday'){
-            alert('Tomorrow is Wednesday!');
-    }   else if(dayName === 'wednesday'){
-            alert('Tomorrow is Thursday!');
-    }   else if(dayName === 'thursday'){
-            alert('Tomorrow is Friday!');
-    }   else if(dayName === 'friday'){
-            alert('Tomorrow is Saturday!');
-    }   else if(dayName === 'saturday'){
-            alert('Tomorrow is Sunday!');
-    }   else{
-            alert('Please type a valid day name.')
-    }
+// if(dayName === 'sunday'){
+//             alert('Tomorrow is Monday');
+//     }   else if(dayName === 'monday'){
+//             alert('Tomorrow is Tuesday!');
+//     }   else if(dayName === 'tuesday'){
+//             alert('Tomorrow is Wednesday!');
+//     }   else if(dayName === 'wednesday'){
+//             alert('Tomorrow is Thursday!');
+//     }   else if(dayName === 'thursday'){
+//             alert('Tomorrow is Friday!');
+//     }   else if(dayName === 'friday'){
+//             alert('Tomorrow is Saturday!');
+//     }   else if(dayName === 'saturday'){
+//             alert('Tomorrow is Sunday!');
+//     }   else{
+//             alert('Please type a valid day name.')
+//     }
+
+switch (dayName) {
+    case 'sunday':
+        alert('Tomorrow is Monday');
+            break;
+    case 'monday':
+        alert('Tomorrow is Tuesday!');
+            break;
+    case 'tuesday':
+        alert('Tomorrow is Wednesday!');
+            break;
+    case 'wednesday':
+        alert('Tomorrow is Thursday!');
+            break;
+    case 'thursday':
+        alert('Tomorrow is Friday!');
+            break;
+    case 'friday':
+        alert('Tomorrow is Saturday!');
+            break;
+    case 'saturday':
+        alert('Tomorrow is Sunday!');
+            break;
+    default:
+        alert('Please type a valid day name.')
+}
