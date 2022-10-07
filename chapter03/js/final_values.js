f1a383a2de55ebe98b6f83e5db308e90e819121a
JamesSenva <jamessenva@gmail.com>
2022-10-07 22:33:39 +0530

    Need to redo this exercise to understand how values work

---
+++ chapter03/js/final_values.js
@@ -0,0 +1,22 @@
+// JavaScript code goes here
+
+// Final values
+
+// Take a look at the following program.
+
+let nb1 = Number(prompt("Enter nb1:"));
+let nb2 = Number(prompt("Enter nb2:"));
+let nb3 = Number(prompt("Enter nb3:"));
+
+if (nb1 > nb2) {
+  nb1 = nb3 * 2;
+} else {
+  nb1++;
+  if (nb2 > nb3) {
+    nb1 += nb3 * 3;
+  } else {
+    nb1 = 0;
+    nb3 = nb3 * 2 + nb2;
+  }
+}
+console.log(nb1, nb2, nb3);
