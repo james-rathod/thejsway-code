f9e51829968b2e52a599297fa17f9d34b79bf64b
JamesSenva <jamessenva@gmail.com>
2022-10-09 19:38:55 +0530

    Show turn number for 10 turns using for and while loop

---
+++ chapter04/js/carousel.js
@@ -0,0 +1,18 @@
+// JavaScript code goes here
+
+// Carousel
+
+// Write a program that launches a carousel for 10 turns, showing the turn number each time.
+
+// When it's done, improve it so that the number of turns is given by the user.
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`This is ${i} turn.`)
+// }
+
+
+let i = 1;
+while(i <= 10){
+    console.log(`This is ${i} turn.`);
+    i++;
+}
