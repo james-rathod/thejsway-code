ec884597273048c5dd95acc5c730e06f0a9fe1ea
JamesSenva <jamessenva@gmail.com>
2022-10-10 22:51:32 +0530

    Basic calculator using fat arrow syntax

--- chapter05/js/calculator.js
+++ chapter05/js/calculator.js
@@ -5,8 +5,21 @@
 // Complete the following program so that it offers the four basic arithmetical operations: addition, subtraction, multiplication and division. You can use either a function declaration or a function expression.
 
 // TODO: complete program
+// function declaration
+// function calculate(num1, operator, num2){
+//     if(operator === '+'){
+//         return num1 + num2;
+//     }else if(operator === '-'){
+//         return num1 - num2;
+//     }else if(operator === '*'){
+//         return num1 * num2;
+//     }else if(operator === '/'){
+//         return num1 / num2;
+//     }
+// }
 
-function calculate(num1, operator, num2){
+// function expression
+let calculate = (num1, operator, num2) => {
     if(operator === '+'){
         return num1 + num2;
     }else if(operator === '-'){
