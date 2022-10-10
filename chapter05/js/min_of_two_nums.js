76f5f96fadf8816c3fa5f238debf56cf1f648903
JamesSenva <jamessenva@gmail.com>
2022-10-10 22:38:54 +0530

    Show minimum of two numbers using conditions-else/if

---
+++ chapter05/js/min_of_two_nums.js
@@ -0,0 +1,19 @@
+// JavaScript code goes here
+
+// Minimum of two numbers
+
+// Let's pretend the JavaScript Math.min() function doesn't exist. Complete the following program so that the min() function returns the minimum of its two received numbers.
+
+// TODO: write the min() function
+
+let min = (num1, num2) => {
+    if(num1 < num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
+
+console.log(min(4.5, 5)); // Must show 4.5
+console.log(min(19, 9));  // Must show 9
+console.log(min(1, 1));   // Must show 1
