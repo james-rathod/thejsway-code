6e5c16586b863b09c95090a52d247b8949b200b7
JamesSenva <jamessenva@gmail.com>
2022-10-10 22:45:49 +0530

    Basic calculator using conditions-else/if

---
+++ chapter05/js/calculator.js
@@ -0,0 +1,24 @@
+// JavaScript code goes here
+
+// Calculator
+
+// Complete the following program so that it offers the four basic arithmetical operations: addition, subtraction, multiplication and division. You can use either a function declaration or a function expression.
+
+// TODO: complete program
+
+function calculate(num1, operator, num2){
+    if(operator === '+'){
+        return num1 + num2;
+    }else if(operator === '-'){
+        return num1 - num2;
+    }else if(operator === '*'){
+        return num1 * num2;
+    }else if(operator === '/'){
+        return num1 / num2;
+    }
+}
+
+console.log(calculate(4, "+", 6));  // Must show 10
+console.log(calculate(4, "-", 6));  // Must show -2
+console.log(calculate(2, "*", 0));  // Must show 0
+console.log(calculate(12, "/", 0)); // Must show Infinity
