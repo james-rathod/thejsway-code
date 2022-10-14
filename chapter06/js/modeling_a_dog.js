d73f7689e7115a1ff97754679ff43b6611504b7d
JamesSenva <jamessenva@gmail.com>
2022-10-14 23:50:54 +0530

    Create the dog function

---
+++ chapter06/js/modeling_a_dog.js
@@ -0,0 +1,18 @@
+// JavaScript code goes here
+
+// Modeling a dog
+
+// Complete the following program to add the dog object definition.
+
+// TODO: create the dog object here
+const dog = {
+    name: 'Fang',
+    species: 'boarhound',
+    size: '75',
+    bark() {
+        return 'Grrr! Grrr!';
+    }
+};
+
+console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
