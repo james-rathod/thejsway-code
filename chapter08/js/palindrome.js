de791a944c1f7b6dbd76e72c8a2af7b594b1a7b4
JamesSenva <jamessenva@gmail.com>
2022-10-22 23:38:43 +0530

    Show if the word is palindrome or not

---
+++ chapter08/js/palindrome.js
@@ -0,0 +1,77 @@
+// JavaScript code goes here
+
+// JavaScript code goes here
+
+// JavaScript code goes here
+
+
+
+
+// Word info
+// Write a program that asks you for a word then shows its length, lowercase, and uppercase values.
+
+let word = prompt('Please type a word to see it\'s information');
+
+
+let vowelsCount = 0;
+
+function wordInfo(word) {
+
+    console.log(`The length of the word is ${word.length}`);
+    console.log(`Lowercase value: ${word.toLowerCase()}`);
+    console.log(`Uppercase value: ${word.toUpperCase()}`);
+
+    // Vowel count
+    // Improve the previous program so that it also shows the number of vowels inside the word.
+    let wordToLowerCase = word.toLowerCase();
+    let vowels = wordToLowerCase.split('');
+
+    // Backwards word
+    // Improve the previous program so that it shows the word written backwards.
+    let reversedWord = vowels.reverse().join('');
+
+
+    //     Palindrome
+    // Improve the previous program to check if the word is a palindrome. A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+    //     "radar" should be detected as a palindrome, "Radar" too.
+
+
+    
+    vowels.forEach(letter => {
+        if(letter === 'a'){
+            vowelsCount++;
+        }
+        if(letter === 'e'){
+            vowelsCount++;
+        }
+        if(letter === 'i'){
+            vowelsCount++;
+        }
+        if(letter === 'o'){
+            vowelsCount++;
+        }
+        if(letter === 'u'){
+            vowelsCount++;
+        }
+    }); 
+
+    console.log(reversedWord);
+
+    if(word === reversedWord){
+        console.log(`It's palindrome: ${word} <-> ${reversedWord}`);
+    }else{
+        console.log(`It's not a palindrome: ${word} x ${reversedWord}`)
+    }
+
+
+}
+
+
+
+
+
+
+
+wordInfo(word);
+console.log(`Total vowels in the word is ${vowelsCount}`);
